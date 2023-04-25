c57f7f4cdcb2ee104de5caaa346daed72b3b7726
Zerlaxv1 <nino.duduch@gmail.com>
2023-04-25 02:49:54 +0200

    Created dataaccess folder and moved the CallApi methods to dataaccess.  Created a flag to indicate if the GridSearchResultsListBox should be visible, in the viewmodel.  Forced the Textbox gotfocus and lostfocus events to call methods in the view model.  Moved the collection of strings to the viewmodel

--- Models/MyItems.cs
+++ Models/MyItems.cs
@@ -4,8 +4,17 @@ namespace Avalonia_RandomAnimeTorrentApp.Models
 {
     public class MyItem
     {
+        /// <summary>
+        /// The Ttile of the item
+        /// </summary>
         public string Text { get; set; }
+        /// <summary>
+        /// The description of the item, the links to the torrent and the image etc...
+        /// </summary>
         public string[] Tags { get; set; }
+        /// <summary>
+        /// The image of the item that will be displayed
+        /// </summary>
         public Avalonia.Media.Imaging.Bitmap ImageBitmap { get; set; }
 
     }
--- ViewModels/SearchAndInfoViewModel.cs
+++ ViewModels/SearchAndInfoViewModel.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Avalonia_RandomAnimeTorrentApp.ViewModels
+namespace Avalonia_RandomAnimeTorrentApp.ViewModels 
 {
-    internal class SearchAndInfoViewModel
+    internal partial class SearchAndInfoViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private List<String> searchResults = new List<String>();
 
+        [ObservableProperty]
+        private System.Timers.Timer searchTimer;
+
+        [ObservableProperty]
+        private string greeting = "Present ?";
+
+        [ObservableProperty]
+        private bool isGridListBoxVisible = false;
+
+        [ObservableProperty]
+        public string searchText;
+
+        [ObservableProperty]
+        private string myItems;
+
+        [ObservableProperty]
+        private string imageUrl = "https://i.imgur.com/1Z1Z1Z1.png";
+
+        [RelayCommand]
+        private void TextBoxGotFocus() => isGridListBoxVisible = true;
+
+        [RelayCommand]
+        private void TextBoxLostFocus() => isGridListBoxVisible = false;
+
+        [RelayCommand]
+        private void onSearchResulteSelectionChangedListBox() { }
     }
 }
