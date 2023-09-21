9f886c9a4f2850fca402a48f64e9a7b8516a728b
Zerlaxv1 <nino.duduch@gmail.com>
2023-09-21 17:41:02 +0200

    new view handling

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,28 +1,52 @@
 ﻿using Avalonia_RandomAnimeTorrentApp.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
+using System.Collections.Generic;
+using System;
 using System.IO;
 
 namespace Avalonia_RandomAnimeTorrentApp.ViewModels
 {
     public partial class MainWindowViewModel : ObservableObject
     {
+        private Dictionary<Type, object> viewModelCache = new Dictionary<Type, object>();
+
         [ObservableProperty]
-        private object content;
-
-        readonly SearchAndInfoViewModel saivm = new();
-
-        readonly PlayerViewModel pvm = new();
+        private object? content;
 
         public MainWindowViewModel()
         {
-            Content = saivm;
+            ChangeContent<SearchAndInfoViewModel>();
 
             WeakReferenceMessenger.Default.Register<Stream>(this, (r, m) =>
             {
-                Content = pvm;
+                ChangeContent<PlayerViewModel>();
             });
         }
-        
+        private void ChangeContent<T>() where T : class
+        {
+            Type type = typeof(T);
+
+            if (viewModelCache.TryGetValue(type, out object? cachedViewModel))
+            {
+                // Use the cached ViewModel
+                Content = cachedViewModel;
+            }
+            else
+            {
+                // Create a new ViewModel
+                if (type == typeof(SearchAndInfoViewModel))
+                {
+                    viewModelCache[type] = new SearchAndInfoViewModel();
+                }
+                else if (type == typeof(PlayerViewModel))
+                {
+                    viewModelCache[type] = new PlayerViewModel();
+                }
+
+                Content = viewModelCache[type];
+            }
+        }
+
     }
 }
